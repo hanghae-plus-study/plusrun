83b0b5ec37544228249dac9db77f241373e45ce0
uuyeong <proyeongs@gmail.com>
2024-11-30 19:20:30 +0900

    feat: 커뮤니티 포스팅 페이지 추가

--- src/lib/router/lazyPages.ts
+++ src/lib/router/lazyPages.ts
@@ -5,4 +5,5 @@ export const SignupPage = lazy(() => import("../../pages/signup"));
 export const HomePage = lazy(() => import("../../pages/home"));
 export const CoursePage = lazy(() => import("../../pages/course"));
 export const CommunityPage = lazy(() => import("../../pages/community"));
+export const CommunityPostingPage = lazy(() => import("../../pages/community-posting"));
 export const RootPage = lazy(() => import("../../layout/Root"));
---
+++ src/pages/community-posting/services/usePostForm.ts
@@ -0,0 +1,84 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import supabase from "../../../lib/supabase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AuthError } from "@supabase/supabase-js";
+import { CategoryType } from "@/src/types/posts";
+
+type PostFormInputs = {
+  title: string;
+  content: string;
+  category: CategoryType;
+};
+
+const usePostForm = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostFormInputs>();
+  const [serverError, setServerError] = useState<string | null>(null);
+  const validationRules = {
+    title: {
+      required: "제목을 입력해주세요.",
+      minLength: {
+        value: 5,
+        message: "제목을 최소 5글자 이상 입력해주세요.",
+      },
+      maxLength: {
+        value: 20,
+        message: "제목을 최대 20글자 이상 입력해주세요.",
+      },
+    },
+    content: {
+      required: "내용을 입력해주세요.",
+      minLength: {
+        value: 5,
+        message: "내용은 최소 5글자 이상 입력해주세요.",
+      },
+    },
+  };
+
+  const onSubmit: SubmitHandler<PostFormInputs> = async (formData) => {
+    try {
+      setServerError(null);
+      // TODO : rest api로 변경하기
+      const userInfo = await supabase.auth.getUser();
+      const { error, status } = await supabase.from("community_posts").insert([
+        {
+          author_id: userInfo.data.user?.id,
+          title: formData.title,
+          content: formData.content,
+          category: formData.category,
+        },
+      ]);
+
+      if (error) {
+        setServerError(error.message);
+      }
+      if (status === 201) {
+        navigate("/community");
+      }
+    } catch (error) {
+      if (error instanceof AuthError) {
+        setServerError(error.message);
+        return;
+      }
+      toast("오류가 발생했습니다.");
+    }
+  };
+
+  return {
+    register,
+    handleSubmit,
+    formError: errors,
+    onSubmit,
+    validationRules,
+    error: serverError,
+  };
+};
+
+export default usePostForm;
